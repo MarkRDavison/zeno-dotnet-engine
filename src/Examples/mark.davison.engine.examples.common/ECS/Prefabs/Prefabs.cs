5df5729a49bbd3e2930f3449e41cfe90a35b26d9
mark.davison <markdavison0+github@gmail.com>
2023-10-01 17:11:03 +1300

    prefab

---
+++ src/Examples/mark.davison.engine.examples.common/ECS/Prefabs/Prefabs.cs
@@ -0,0 +1,10 @@
+﻿namespace mark.davison.engine.examples.common.ECS.Prefabs;
+
+public static class Prefabs
+{
+    public static IEntity CreatePlayerShip()
+    {
+        var e = new Entity();
+        return e;
+    }
+}
